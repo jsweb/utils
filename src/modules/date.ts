9a4e4124da6e3926c1f5c398a2e4d315200b5866
Alex Bruno Cáceres <contato@alexbruno.dev>
2023-04-26 22:31:39 -0300

    Increment date module

--- src/modules/date.ts
+++ src/modules/date.ts
@@ -1,4 +1,4 @@
-interface CurrentDate {
+interface CurrentDateTime {
   date: Date
   unix: number
   json: string
@@ -6,14 +6,14 @@ interface CurrentDate {
 
 /**
  * Gets current date and time in 3 basic formats: Date, Unix and JSON.
- * Returns a DateNow object.
- * @return {*} {DateNow} DateNow object
+ * Returns a CurrentDateTime object: { date: Date, unix: Number, json: String }.
+ * @return {*} {CurrentDateTime} CurrentDateTime object
  * @example
  * const date = getCurrentDate()
  * console.log(date)
  * // { date: 2021-08-15T18:00:00.000Z, unix: 1629043200000, json: '2021-08-15T18:00:00.000Z' }
  */
-export function getCurrentDate(): CurrentDate {
+export function getCurrentDateTime(): CurrentDateTime {
   const date = new Date()
   const unix = date.getTime()
   const json = date.toJSON()
@@ -22,11 +22,11 @@ export function getCurrentDate(): CurrentDate {
 
 /**
  * Receives a Date object and adds a given amount of days to it.
- * Returns a new DateNow object.
+ * Returns a CurrentDateTime object: { date: Date, unix: Number, json: String }.
  *
- * @param {Date} date Date to add days to
+ * @param {Date|number|string} date Date to add days to
  * @param {number} days Amount of days to add
- * @return {*} {DateNow} New DateNow object
+ * @return {*} {CurrentDateTime} CurrentDateTime object
  * @example
  * const date = new Date()
  * const newDate = addDays(date, 5)
@@ -36,12 +36,42 @@ export function getCurrentDate(): CurrentDate {
 export function addDays(
   date: Date | number | string,
   days: number
-): CurrentDate {
-  const newDate = new Date(date)
-  newDate.setDate(newDate.getDate() + days)
+): CurrentDateTime {
+  const dt = new Date(date)
+  const result = dt.getDate() + days
 
-  const unix = newDate.getTime()
-  const json = newDate.toJSON()
+  dt.setDate(result)
 
-  return { date: newDate, unix, json }
+  const unix = dt.getTime()
+  const json = dt.toJSON()
+
+  return { date: dt, unix, json }
+}
+
+/**
+ * Receives a Date object and subtracts a given amount of days from it.
+ * Returns a CurrentDateTime object: { date: Date, unix: Number, json: String }.
+ *
+ * @param {Date|number|string} date Date to subtract days from
+ * @param {number} days Amount of days to subtract
+ * @return {*} {CurrentDateTime} CurrentDateTime object
+ * @example
+ * const date = new Date()
+ * const newDate = subtractDays(date, 5)
+ * console.log(newDate)
+ * // { date: 2021-08-10T18:00:00.000Z, unix: 1628620800000, json: '2021-08-10T18:00:00.000Z' }
+ */
+export function subtractDays(
+  date: Date | number | string,
+  days: number
+): CurrentDateTime {
+  const dt = new Date(date)
+  const result = dt.getDate() - days
+
+  dt.setDate(result)
+
+  const unix = dt.getTime()
+  const json = dt.toJSON()
+
+  return { date: dt, unix, json }
 }
--- tests/modules/date.ts
+++ tests/modules/date.ts
@@ -1,17 +1,29 @@
 import test from 'ava'
-import { getCurrentDate, addDays } from '../../src/modules/date'
+import {
+  getCurrentDateTime,
+  addDays,
+  subtractDays,
+} from '../../src/modules/date'
 
-test('getCurrentDate', (t) => {
-  const dt = getCurrentDate()
+test('getCurrentDateTime', (t) => {
+  const dt = getCurrentDateTime()
   t.is(dt.date instanceof Date, true)
   t.is(typeof dt.unix, 'number')
   t.is(typeof dt.json, 'string')
 })
 
 test('addDays', (t) => {
-  const dt = new Date()
-  const newDate = addDays(dt, 5)
-  t.is(newDate.date instanceof Date, true)
-  t.is(typeof newDate.unix, 'number')
-  t.is(typeof newDate.json, 'string')
+  const dt = getCurrentDateTime()
+  const result = addDays(dt.unix, 5)
+  t.is(result.date instanceof Date, true)
+  t.is(typeof result.unix, 'number')
+  t.is(typeof result.json, 'string')
+})
+
+test('subtractDays', (t) => {
+  const dt = getCurrentDateTime()
+  const result = subtractDays(dt.unix, 5)
+  t.is(result.date instanceof Date, true)
+  t.is(typeof result.unix, 'number')
+  t.is(typeof result.json, 'string')
 })
